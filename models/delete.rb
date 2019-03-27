37ccb653b721859d333fca72185c01237009c97b
Illya Moskvin <imoskvin@artic.edu>
2019-03-27 10:11:43 -0500

    Fix malformed UUIDs in deletes endpoint
    Some had `/files/access_master` suffix.

--- models/delete.rb
+++ models/delete.rb
@@ -12,7 +12,7 @@ class Delete < BaseModel
     data.extend LakeUnwrapper
 
     ret = {}
-    ret[:lake_guid] = data.get(:id, false)
+    ret[:lake_guid] = data.get(:id, false).split(/\//).first
     ret[:indexed_at] = data.get(:timestamp, false).to_time(:utc).iso8601 rescue nil
 
     ret
