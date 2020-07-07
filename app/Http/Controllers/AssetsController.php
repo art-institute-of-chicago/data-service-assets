5a8e2e3f026a4a7740090cea8df3890306af7800
Illya Moskvin <ivmoskvin@gmail.com>
2020-07-07 15:07:52 -0500

    Clean out leftover boilerplate

--- app/Http/Controllers/AssetsController.php
+++ app/Http/Controllers/AssetsController.php
@@ -2,11 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Foo;
-use App\Bar;
-
-use Illuminate\Http\Request;
-
 use Aic\Hub\Foundation\AbstractController as BaseController;
 
 class AssetsController extends BaseController
