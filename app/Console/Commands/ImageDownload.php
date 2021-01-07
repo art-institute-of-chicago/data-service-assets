bb43829362b4b7e499271b8b66fdcfb8f6856ce6
Illya Moskvin <imoskvin@artic.edu>
2021-01-07 02:18:28 -0600

    Add missing `fetch` method [WEB-1916]

--- app/Console/Commands/ImageDownload.php
+++ app/Console/Commands/ImageDownload.php
@@ -77,4 +77,16 @@ class ImageDownload extends AbstractCommand
         }
     }
 
+    private function fetch($file, &$headers = null)
+    {
+        if (!$contents = @file_get_contents($file)) {
+            throw new \Exception('Fetch failed: ' . $file);
+        }
+
+        if (isset($http_response_header)) {
+            $headers = $http_response_header;
+        }
+
+        return $contents;
+    }
 }
