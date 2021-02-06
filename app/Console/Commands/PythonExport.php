2d96b717dbca1d7c9aee52f1203d6aa6b23b459f
Illya Moskvin <imoskvin@artic.edu>
2021-02-06 00:49:49 -0600

    Copy python commands from image service [WEB-1916]

---
+++ app/Console/Commands/PythonExport.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Image;
+
+use League\Csv\Writer;
+
+class PythonExport extends AbstractCommand
+{
+
+    protected $signature = 'python:export';
+
+    protected $description = 'Export CSV of image ids for Python processing';
+
+    public function handle()
+    {
+        $path = storage_path() . '/app/python-input.csv';
+
+        $csv = Writer::createFromPath($path, 'w');
+
+        $csv->insertOne([
+            'id',
+            'ahash',
+            'dhash',
+            'phash',
+            'whash',
+            'colorfulness',
+        ]);
+
+        // Only target images that have been downloaded
+        $images = Image::whereNotNull('image_downloaded_at');
+
+        // Only target images that are missing fields provided by Python
+        $images = $images->where(function($query) {
+            $query->whereNull('ahash')
+                ->orWhereNull('dhash')
+                ->orWhereNull('phash')
+                ->orWhereNull('whash')
+                ->orWhereNull('colorfulness');
+        });
+
+        if (!$this->confirm($images->count() . ' images will be exported. Proceed?'))
+        {
+            return;
+        }
+
+        foreach ($images->cursor() as $image)
+        {
+            $row = [
+                'id' => $image->id,
+                'ahash' => isset($image->ahash) ? null : true,
+                'dhash' => isset($image->dhash) ? null : true,
+                'phash' => isset($image->phash) ? null : true,
+                'whash' => isset($image->whash) ? null : true,
+                'colorfulness' => isset($image->colorfulness) ? null : true,
+            ];
+
+            $csv->insertOne($row);
+
+            $this->info(json_encode($row));
+        }
+    }
+
+}
---
+++ app/Console/Commands/PythonImport.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Image;
+
+use League\Csv\Reader;
+
+class PythonImport extends AbstractCommand
+{
+
+    protected $signature = 'python:import';
+
+    protected $description = 'Import CSV for image metadata';
+
+    public function handle()
+    {
+        $path = storage_path() . '/app/python-output.csv';
+
+        $csv = Reader::createFromPath($path, 'r');
+        $csv->setHeaderOffset(0);
+
+        foreach ($csv->getRecords() as $row)
+        {
+            $image = Image::find($row['id']);
+
+            if( !$image ) {
+                $this->info("{$row['id']} - not found");
+                continue;
+            }
+
+            // https://github.com/JohannesBuchner/imagehash
+            !empty($row['ahash']) && $image->ahash = $row['ahash'];
+            !empty($row['dhash']) && $image->dhash = $row['dhash'];
+            !empty($row['phash']) && $image->phash = $row['phash'];
+            !empty($row['whash']) && $image->whash = $row['whash'];
+            !empty($row['colorfulness']) && $image->colorfulness = $row['colorfulness'];
+
+            $image->save();
+
+            // Output for reference
+            $this->info("{$image->id} - updated");
+        }
+    }
+
+}
