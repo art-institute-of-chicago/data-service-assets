69330b32fa854f18c81f936354c7ab7fb2bab44f
Illya Moskvin <imoskvin@artic.edu>
2021-04-08 12:57:58 -0500

    Remove default values from config/source.php [WEB-1835]

--- config/source.php
+++ config/source.php
@@ -1,25 +1,14 @@
 <?php
 
 return [
+    'api_url' => env('SOURCE_API_URL'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | API URL
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the URL of where to get source information from.
-    | Set this in your ".env" file.
-    |
-    */
-
-    'api_url' => env('SOURCE_API_URL', 'http://exampleapi.source.com/'),
-
-    'username' => env('SOURCE_USERNAME', 'username'),
-    'password' => env('SOURCE_PASSWORD', '********'),
+    'username' => env('SOURCE_USERNAME'),
+    'password' => env('SOURCE_PASSWORD'),
 
     'iiif_url' => env('IIIF_URL', 'https://www.artic.edu/iiif/2'),
 
-    'uuid_prefix' => env('UUID_PREFIX', ''),
+    'uuid_prefix' => env('UUID_PREFIX'),
 
     'python_chunk_size' => is_numeric(env('PYTHON_CHUNK_SIZE')) ? (int) env('PYTHON_CHUNK_SIZE') : null,
 ];
