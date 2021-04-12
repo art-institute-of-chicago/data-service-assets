c73cb75843618b4e0cc24269ddc34e00102dfaba
Illya Moskvin <imoskvin@artic.edu>
2021-04-12 14:53:13 -0500

    Add `invalidate` command [WEB-99]

---
+++ config/cloudfront.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'key' => env('AWS_KEY'),
+    'secret' => env('AWS_SECRET'),
+
+    'region' => env('AWS_REGION', 'us-east-1'),
+    'sdk_version' => env('AWS_SDK_VERSION', '2016-01-13'),
+
+    'distribution' => env('AWS_CLOUDFRONT_DISTRIBUTION'),
+];
